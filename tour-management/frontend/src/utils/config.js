4eee13fb67a12c823bca88657538c4ef3545483e
Shikhar-Rastogi <shikharrastogi021@gmail.com>
2026-01-08 20:13:49 +0530

    Deployment Changes

--- tour-management/frontend/src/utils/config.js
+++ tour-management/frontend/src/utils/config.js
@@ -1,2 +1,6 @@
-const BASE_URL = 'https://take-my-trip.onrender.com/api/v1';
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://take-my-trip.onrender.com/api/v1"
+    : "http://localhost:4000/api/v1";
+
 export default BASE_URL;
